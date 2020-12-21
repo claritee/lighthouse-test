b663a5148ccc3145b2874724fe872bd3f8cdfe57
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2020-12-21 13:24:38 +1100

    Added ability to insert metrics to db

--- auth-json.js
+++ auth-json.js
@@ -1,13 +1,24 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
+const { v4: uuidv4 } = require('uuid');
+const { Client } = require('pg');
 
 const HEADLESS = process.env.HEADLESS == "true" ? true : false
 const PORT = process.env.PORT || 8041; //debugging port
 const EMAIL = process.env.EMAIL || ''
 const PASSWORD = process.env.PASSWORD || ''
-const LOGINURL = process.env.LOGIN_URL || ''
-const TARGETURL = process.env.TARGET_URL || ''
+const LOGIN_URL = process.env.LOGIN_URL || ''
+const TARGET_URL = process.env.TARGET_URL || ''
+
+const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost'
+const POSTGRES_USER = process.env.POSTGRES_USER || ''
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || ''
+const POSTGRES_DB = process.env.POSTGRES_DB || ''
+const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432
+
+const BUILD_COMMIT = process.env.BUILD_COMMIT || ''
+const BUILD_NUMBER = process.env.BUILD_NUMBER || ''
 
 /**
  * @param {import('puppeteer').Browser} browser
@@ -47,6 +58,50 @@ async function logout(browser, origin) {
   await page.close();
 }
 
+function extractResultMetrics(lighthouseResult) {
+  const { audits } = lighthouseResult;
+  const metrics = {}
+  const metricKeys = [
+    'first-contentful-paint',
+    'speed-index',
+    'largest-contentful-paint',
+    'interactive',
+    'total-blocking-time',
+    'cumulative-layout-shift',
+  ]
+
+  metricKeys.forEach((metricKey) => {
+    metrics[metricKey] = audits[metricKey];
+  });
+
+  console.log(metrics);
+
+  return metrics;
+}
+
+async function insertMetric(dbClient, targetDomain, path, metricType, metricValue, commitHash, buildNumber, createdAt) {
+  const id = uuidv4();
+
+  const text = "INSERT INTO lighthouse_metrics(id, target_domain, path, metric_type, metric_value, commit_hash, build_number, created_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8);"
+  const values = [
+    id,
+    targetDomain,
+    path,
+    metricType,
+    metricValue,
+    commitHash,
+    buildNumber,
+    createdAt
+  ];
+
+  try {
+    await dbClient.query(text, values);
+  } catch (e) {
+    console.log(`Error storing lighthouse metric: ${e.message}\nText: ${text}\nValues: ${values}`);
+    throw e
+  }
+}
+
 async function main() {
   const browser = await puppeteer.launch({
     args: [`--remote-debugging-port=${PORT}`, `--no-sandbox`],
@@ -55,35 +110,58 @@ async function main() {
   });
 
   // Setup the browser session to be logged into our site.
-  await login(browser, LOGINURL);
+  await login(browser, LOGIN_URL);
 
-  const url = TARGETURL;
+  // Run lighthouse
+  const url = TARGET_URL;
   const options = {port: PORT, disableStorageReset: true, output: 'json'};
   const result = await lighthouse(url, options);
 
+  // Cleanup
   await browser.close();
 
+  // Parse results
   const lighthouseResult = result.lhr;
-
   const metrics = extractResultMetrics(lighthouseResult)
 
-  console.log(metrics);
-
+  // Store artifacts to filesystem
   fs.writeFileSync('report.json', JSON.stringify(lighthouseResult, null, 2));
   fs.writeFileSync('metrics.json', JSON.stringify(metrics, null, 2));
-}
 
-function extractResultMetrics(lighthouseResult) {
-  const { audits } = lighthouseResult;
+  // Get DB client
+  const dbClient = new Client({
+    user: POSTGRES_USER,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: POSTGRES_PORT,
+  });
+  await dbClient.connect();
 
-  return {
-    firstContentfulPaint: audits['first-contentful-paint'],
-    speedIndex: audits['speed-index'],
-    largestContentfulPaint: audits['largest-contentful-paint'],
-    timeToInteractive: audits['interactive'],
-    totalBlockingTime: audits['total-blocking-time'],
-    cumulativeLayoutShift: audits['cumulative-layout-shift'],
+  const timeNow = (new Date()).toISOString();
+
+  // Iterate each metric, and store to db
+  const keys = Object.keys(metrics);
+
+  for (let idx = 0; idx < keys.length; idx++) {
+    const metricKey = keys[idx];
+    const metric = metrics[metricKey];
+
+    // TODO: Split TARGET_URL to TARGET_DOMAIN and PATH
+    await insertMetric(
+      dbClient,
+      TARGET_URL,
+      TARGET_URL,
+      metricKey,
+      metric['numericValue'].toFixed(4),
+      BUILD_COMMIT,
+      BUILD_NUMBER,
+      timeNow
+    );
   }
+
+  // Release db connection
+  await dbClient.end();
 }
 
 if (require.main === module) {
